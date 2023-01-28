617bf603fb94aeded591263ceadc26694943b6af
Till Harbaum <till@harbaum.org>
2023-01-28 17:08:00 +0100

    Temporary hack around issue #3

--- galagino/emulation.c
+++ galagino/emulation.c
@@ -443,7 +443,7 @@ unsigned char RdZ80(unsigned short Addr) {
 }
 
 void prepare_emulation(void) {
-  memory = malloc(8192);
+  memory = malloc(16384); // TODO: Fixes #3, but 8192 should be sufficient
 
   for(current_cpu=0;current_cpu<3;current_cpu++)
     ResetZ80(&cpu[current_cpu]);
